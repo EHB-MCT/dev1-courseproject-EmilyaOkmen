1ed92d90a883ae479bdff7043106e92fe69c3eb0
EmilyaOkmen <emilyaokmen@student.ehb.be>
2024-11-12 16:26:29 +0100

    SecondCommit

--- milestones/generativedesign.js
+++ milestones/generativedesign.js
@@ -10,13 +10,13 @@ let height = context.canvas.height;
 
 draw();
 
-function draw(){
+function draw() {
 
-    context.fillStyle="black"
+    context.fillStyle = "#ADD8E6"
     context.fillRect(0, 0, width, height);
-    
 
-    drawRandomShapes(20); 
+
+    drawRandomShapes(20);
 }
 
 function drawRandomShapes(dragonEye) {
@@ -24,23 +24,46 @@ function drawRandomShapes(dragonEye) {
 
 
     for (let i = 0; i < dragonEye; i++) {
-    
+        
         let x = Math.random() * canvas.width;
         let y = Math.random() * canvas.height;
+
+            
         
+        let randomColor1 = Utils.rgb(Utils.randomNumber(0, 255), Utils.randomNumber(0, 255), Utils.randomNumber(0, 255));
+        let randomColor2 = Utils.rgb(Utils.randomNumber(0, 255), Utils.randomNumber(0, 255), Utils.randomNumber(0, 255));
+        let randomColor3 = Utils.rgb(Utils.randomNumber(0, 255), Utils.randomNumber(0, 255), Utils.randomNumber(0, 255));
+
+             context.fillStyle = randomColor1;
+             Utils.fillCircle(x, y, 50);
+     
+             context.fillStyle = randomColor2;
+             Utils.fillEllipse(x, y, 25 * 2, 20);
+     
+             context.fillStyle = randomColor3;
+             Utils.fillEllipse(x, y, 50 / 2, 20 * 2);
+
+        /*let x = Math.random() * canvas.width;
+        let y = Math.random() * canvas.height;
+
+
+
         // Draw circle
-        context.fillStyle = "red";
+        context.fillStyle = "#E55451";
         Utils.fillCircle(x, y, 50);
-        
+
         // Draw ellipse
-        context.fillStyle = "green";
+        context.fillStyle = "#F2D4D7";
         Utils.fillEllipse(x, y, 25 * 2, 20);
-        
+
         // Draw other ellipse
-        context.fillStyle = "black";
-        Utils.fillEllipse(x, y, 50 / 2, 20 * 2);
+        context.fillStyle = "#000000";
+        Utils.fillEllipse(x, y, 50 / 2, 20 * 2);*/
+
     }
+
 }
+
 
 
 
