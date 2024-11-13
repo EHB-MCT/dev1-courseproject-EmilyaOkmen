2426b99a51139f2c3d032edef4835d130ef55157
EmilyaOkmen <emilyaokmen@student.ehb.be>
2024-11-13 16:12:53 +0100

    CompleteMilestone2

--- milestones/generativedesign.js
+++ milestones/generativedesign.js
@@ -21,27 +21,35 @@ function draw() {
 
 function drawRandomUfos() {
 
+        //kolom maken met rijen, horizontaal en verticaal 
     const columns = 15; 
-    const rows = 15;    
+    const rows = 15;
     const shapeWidth = width / columns;
     const shapeHeight = height / rows;
 
+    //loop die kleuren gaat veranderen voor kolom en rijen
+    // j is voor de rijen en i is voor de kolommen
     for (let i = 0; i < columns; i++) {
         for (let j = 0; j < rows; j++) {
-            let x = i * shapeWidth + shapeWidth / 2;
-            let y = j * shapeHeight + shapeHeight / 2;
+        let x = i * shapeWidth + shapeWidth / 2;
+        let y = j * shapeHeight + shapeHeight / 2;  //even gepositioneerd
 
-let randomColor2 = Utils.rgb(Utils.randomNumber(200, 255), Utils.randomNumber(150, 200), Utils.randomNumber(150, 200));
-let randomColor1 = Utils.rgb(Utils.randomNumber(100, 160), Utils.randomNumber(140, 190), Utils.randomNumber(200, 240)); 
+            //random pastel kleuren
+            let randomColor2 = Utils.rgb(Utils.randomNumber(200, 255),
+                Utils.randomNumber(150, 200), Utils.randomNumber(150, 200));//rose kleur
 
-context.fillStyle = randomColor1;
-Utils.fillEllipse(x, y, shapeWidth / 2, shapeHeight / 4);
+            let randomColor1 = Utils.rgb(Utils.randomNumber(100, 160),
+                Utils.randomNumber(140, 190), Utils.randomNumber(200, 240));//blauwe kleur
 
+            //ELLIPSE
+            context.fillStyle = randomColor1;
+            Utils.fillEllipse(x, y, shapeWidth / 2, shapeHeight / 4);
 
-context.fillStyle = randomColor2;
-Utils.fillEllipse(x, y, shapeWidth / 4, shapeHeight / 2);
+            //ELLIPSE/CIRKEL
+            context.fillStyle = randomColor2;
+            Utils.fillEllipse(x, y, shapeWidth / 4, shapeHeight / 2);
 
         }
     }
- }
+}
 
