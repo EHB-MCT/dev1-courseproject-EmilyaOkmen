f7876944c41cf095e0813dfe43f14694481d1b1c
EmilyaOkmen <emilya.okmen@student.ehb.be>
2024-12-19 00:36:22 +0100

    ufos-maken

--- milestones/generativedesign.js
+++ milestones/generativedesign.js
@@ -34,49 +34,23 @@ function mouseMove(eventData) {
 
 }
 
-function draw() {
+function createUfo() {
+    let size = Math.random() * 50 + 20;
+    return {
+        x: Math.random() * (width + 600) - 600,
+        y: 0 - size
+        size: size,
+        speed: Math.random() * 4 + 1,
+        randomColor1: Utils.rgb(Utils.randomNumber(100, 160), Utils.randomNumber(140, 190), Utils.randomNumber(200, 240)), // Blouwe kleur
+        randomColor2: Utils.rgb(Utils.randomNumber(200, 255), Utils.randomNumber(150, 200), Utils.randomNumber(150, 200)), // Rose kleur
 
-    context.fillStyle = "#fff981"
-    context.fillRect(0, 0, width, height);
+    };
 }
 
 
-function drawRandomUfos() {
-
-    //kolom maken met rijen, horizontaal en verticaal 
-    const columns = 15;
-    const rows = 15;
-    const shapeWidth = width / columns;
-    const shapeHeight = height / rows;
-
-    //loop die kleuren gaat veranderen voor kolom en rijen
-    // j is voor de rijen en i is voor de kolommen
-    for (let i = 0; i < columns; i++) {
-        for (let j = 0; j < rows; j++) {
-
-            let x = i * shapeWidth + shapeWidth / 2;
-            let y = j * shapeHeight + shapeHeight / 2;//even gepositioneerd
-
-            //random pastel kleuren
-            let randomColor2 = Utils.rgb(Utils.randomNumber(200, 255),
-                Utils.randomNumber(150, 200), Utils.randomNumber(150, 200));//rose kleur
-
-            let randomColor1 = Utils.rgb(Utils.randomNumber(100, 160),
-                Utils.randomNumber(140, 190), Utils.randomNumber(200, 240));//blauwe kleur
-
-            //ELLIPSE
-            context.fillStyle = randomColor1;
-            Utils.fillEllipse(x, y, shapeWidth / 2, shapeHeight / 4);
-
-            //ELLIPSE/CIRKEL
-            context.fillStyle = randomColor2;
-            Utils.fillEllipse(x, y, shapeWidth / 4, shapeHeight / 2);
 
 
 
 
 
-        }
-    }
-}
 
